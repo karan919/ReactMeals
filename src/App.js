aca809a8fc4c10a9a2dd0405c37df751a7e7445d
Karan Mahajan <karanmahajan919@gmail.com>
2022-05-13 12:19:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import AvailableMeals from "./Components/AvailableMeals";
 import MealsSummary from "./Components/MealsSummary";
 import Navbar from "./Components/Navbar";
 import Meals from "./images/meals.jpg";
-import KartContextProvider from "./Components/KartContext";
+import { KartContextProvider } from "./Components/KartContext";
+import Cart from "./Components/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
     <KartContextProvider>
-      <Navbar />
+      <Navbar showCartHandler={showCartHandler}/>
       <div className="main-image">
         <img src={Meals} alt="meal" />
       </div>
       <MealsSummary />
       <AvailableMeals />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
     </KartContextProvider>
   );
 }
